071bb7f2f83b5d44e8c0654d250fa8dedb52b202
Gema Gonzalez Mazarias <gegonmaz@gamma40.aulas.gsyc.urjc.es>
2016-10-10 19:02:11 +0200

    pasamos el pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
-    """
-    Clase para leer archivos multimedia
-    """
 
     def __init__ (self):
-        """
-        Constructor. Inicializamos las variables 
-        (Atributos que hay que tener en cuenta)
-        """
         self.width = ""
         self.height = ""
         self.background_color = ""
@@ -24,83 +17,74 @@ class SmallSMILHandler(ContentHandler):
         self.right = ""
         self.src = ""
         self.region = ""
-        self.begin =""
+        self.begin = ""
         self.dur = ""
-        self.misdatos=[]    #crearemos una lista para meter todos los atributos
-        """
-        No crearemos etiquetas de flag, porque el SMIL, no tiene informacion
-        en los atributos
-        """
+        self.misdatos = []
 
     def startElement(self, name, attrs):
-        """
-        Método que se llama cuando se abre una etiqueta
-        """
+
         if name == 'root-layout':
             self.root_layout = {}
             self.misdatos.append(name)
             # De esta manera tomamos los valores de los atributos
-            self.widht = attrs.get('widht',"")
-            self.height = attrs.get('height',"")
-            self.background_color = attrs.get('background-color',"") 
-            self.root_layout=['widht'+' = ' + self.width,'height'+' = ' + 
-self.height,'backgroundcolor'+' = ' + self.background_color]
-            self.misdatos.append(self.root_layout)           
+            self.widht = attrs.get('widht', "")
+            self.height = attrs.get('height', "")
+            self.background_color = attrs.get('background-color', "")
+            self.root_layout=['widht' + ' = ' + self.width, 'height' + ' = ' + 
+self.height, 'backgroundcolor' + ' = ' + self.background_color]
+            self.misdatos.append(self.root_layout)
+           
         elif name == 'region':
             self.region = {}
             self.misdatos.append(name)
-            self.id = attrs.get('id',"")
-            self.top = attrs.get('top',"")
-            self.bottoms = attrs.get('bottoms',"")  
-            self.left = attrs.get('left',"")
-            self.right = attrs.get('right',"")
-            self.region=['id'+' = '+self.id,'top'+' = ' +  self.top, 
-'bottom'+ ' = ' + self.bottom,'left' + ' = ' + self.left, 'right' + 
+            self.id = attrs.get('id', "")
+            self.top = attrs.get('top', "")
+            self.bottoms = attrs.get('bottoms', "")
+            self.left = attrs.get('left', "")
+            self.right = attrs.get('right', "")
+            self.region=['id'+' = ' + self.id,'top' + ' = ' +  self.top, 
+'bottom' + ' = ' + self.bottom, 'left' + ' = ' + self.left, 'right' + 
 ' = ' + self.right]
             self.misdatos.append(self.region)
+
         elif name == 'img':
             self.img = {}
             self.misdatos.append(name)
-            self.src = attrs.get('src',"")
-            self.region = attrs.get('region',"")
-            self.begin = attrs.get('begin',"")
-            self.dur = attrs.get('dur',"")
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
             self.img = ['src' + ' = ' + self.src,'region' + ' = ' 
 + self.region, 'begin' + ' = ' + self.begin, 'dur' + ' = ' + self.dur]
             self.misdatos.append(self.img)
+
         elif name == 'audio':
             self.audio = {}
             self.misdatos.append(name)
-            self.src = attrs.get('src',"")
-            self.begin = attrs.get('begin',"")
-            self.dur = attrs.get('dur',"")
+            self.src = attrs.get('src', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
             self.audio=['src' + ' = ' + self.src,'begin' + ' = ' + self.begin,
 'dur' + ' = ' + self.dur]
-            self.misdatos.append(self.audio) 
+            self.misdatos.append(self.audio)
+ 
         elif name == 'textstream':
             self.texstream = {}
             self.misdatos.append(name)
-            self.src = attrs.get('src',"")
-            self.region = attrs.get('region',"")
-            self.texstream = [self.src,self.region]
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            self.texstream = ['srs' + ' = ' + self.src, ' region' + 
+' = ' + self.region]
             self.misdatos.append(self.texstream)
-        """
-        Metodo para guardar los contenidos del texto e imprimirlos por pantalla
-        """
-        #Creamos un array que contenga los datos y los guarde.(metodo anterior)
+
     def get_tags(self):
        
         print (self.misdatos)
         return self.misdatos
-        # y luego los imprime.
+
 if __name__ == "__main__":
-    """
-    Programa principal
-    """
     parser = make_parser()
     sHandler = SmallSMILHandler()
     parser.setContentHandler(sHandler)
     parser.parse(open('karaoke.smil'))
-
     sHandler.get_tags()
-    # hago cosas con mis datos(imprimo por pantalla con el metodo get_tags)
