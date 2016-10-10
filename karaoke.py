10f77fe250297db12d326cfd20be26a9d92649c2
Gema Gonzalez Mazarias <gegonmaz@gamma40.aulas.gsyc.urjc.es>
2016-10-10 19:02:59 +0200

    pasamos el pep8

--- karaoke.py
+++ karaoke.py
@@ -3,70 +3,69 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-
 from smallsmilhandler import SmallSMILHandler
 import sys
 import csv
 import json
+import urllib.request
 
 
 def lista_etiquetas(misdatos):
-    
+
     listado = ""
-    for diccs in misdatos:
-        atrib_line = ""
-    for key in diccs.keys():
-        if key != "name" and diccs[key] != "":
-           atrib_line = atrib_line + key + "=" + diccs[key] + "\t"
-           listado = listado + diccs["name"] + "\t" + atrib_line + "\n"
-    return listado 
+    for etiqueta in misdatos:
+        listado = listado + etiqueta[0]
+        atribs = etiqueta[1].items()
+        for atrib, contenido in atribs:
+            if contenido != "":
+                listado = listado + "\t" + atrib + "=" + '"' + contenido + '"'
+        listado = listado + "\n"
+    return listado
 
-def to_json(ListaDatos, nombreArchivo=""):
 
-    if not nombreArchivo:
-        nombreArchivo = "local.json"
-    else:
-        nombreArchivo = nombreArchivo.replace(".smil", ".json")
-    with open(fich_name, 'w') as archivo_json:
-        json.dump(ListaDatos, archivo_json, indent=4,
-                  separators=(' ', ': '), sort_keys=True)    
+def to_json(misdatos):
+    with open('karaoke.json', 'w') as outfile_json:
+            json.dump(misdatos, outfile_json, sort_keys=True, indent=3, 
+            separators=(' ', ': '))
+
+
+def url_local(misdatos):
+
+    list_url = []
+    for etiqueta in misdatos:
+        atribs = etiqueta[1].items()
+        for atrib, contenido in atribs:
+            if atrib == "src":
+                url = contenido
+                list_url = url.split("/")
+                remoto = list_url[0]
+                if remoto == "http:":
+                    arch_web = urllib.request.urlopen(url)
+                    filename = list_url[-1]
+                    print("Descargado..." + filename)
+                    f = open(filename, "wb")
+                    f.write(arch_web.read())
+                else:
+                    filename = contenido
+                print("Este contenido ya está en local... " + filename)
+
 
 if __name__ == "__main__":
 
-    
-    """
-    Programa principal
-    nos haremos servir del main de samallamilshandler, que tiene ya todo creado,
-    asi solo tendremos que coger el archivo para leerlo-->para ello tendremos
-    que tener en cuenta el importar sys
-    """
-    
-    #Tratamos de leer el archivo que se nos pase
+    fich = sys.argv[1]
     try:
-        """
-        Para ello crearemos un variable de tipo archivo, que guardara el archivo
-        se le pase, para realizar esto, tendremos que utilizar el metodo sys
-        que importamos anteriormente.y como hicimos en el programa de calcplus,
-        fichero = sys.argv[1] utilizaremos el mismo metodo.
-        """
-        Archivo = open(sys.argv[1])
-        """
-        Ahora el programa nos dice que tenemos que tener en cuenta que hay 
-        que especificar el fichero, asi que tendra que saltar un mensaje de
-        error --> "Usege: python3 karaoke.py file.smil".
-        """
+        Archivo = open(fich)
+
     except IndexError:
         sys.exit("Usege: python3 karaoke.py file.smil")
-        #return 'Usege: python3 karaoke.py file.smil' 
+
     parser = make_parser()
     ssHandler = SmallSMILHandler()
     parser.setContentHandler(ssHandler)
     parser.parse(Archivo)
-  
-    tags = ssHandler.get_tags()
-    print(tags)
+    tags = ssHandler.get_tags()    
     result = lista_etiquetas(misdatos)
-    print(result)  
-    
 
-    
+    print(result)
+    to_json(tags)
+    url_local(tags)   
